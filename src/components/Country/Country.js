c08cac3be7be1ff80e2d788c8f5dded387b10eb5
Asif2535 <aiqbal517@gmail.com>
2021-02-27 02:09:52 +0600

    added country

--- src/components/Country/Country.js
+++ src/components/Country/Country.js
@@ -1,10 +1,23 @@
 import React from 'react';
 
 const Country = (props) => {
-    const {name} = props.countryInfo;
+    const {name, population, region, flag} = props.countryInfo;
+    const flagStyle={
+        height:'50px'
+    };
+    const countryStyle={
+        border:'1px solid red',
+        margin:'10px',
+        padding:'10px'
+    };
+    const handleAddCountry = props.handleAddCountry;
     return (
-        <div>
+        <div style={countryStyle}>
             <h4>This is {name}</h4>
+            <img style={flagStyle} src={flag} alt=""/>
+            <p>population: {population}</p>
+            <p><small>Region: {region}</small></p>
+            <button onClick={()=>handleAddCountry(props.countryInfo)}>Add Country</button>
         </div>
     );
 };
